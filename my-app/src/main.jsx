5466326801a032ef7947b88be2ffeb79ec66ffc2
Saheel Metha <methasahil03@gmail.com>
2026-02-24 15:02:03 +0530

    first commit

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,13 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Nag from './nag.jsx'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-
-  <Nag/>
+    <App />
   </StrictMode>,
-)
+);
