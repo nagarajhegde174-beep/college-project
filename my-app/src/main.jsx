af9c4c56b6a02f9527378de4e51cd5cfd820507c
nagaraj <nagarajhegde174@gmail.com>
2026-02-25 14:36:29 +0530

    2nd commit

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Nag from './nag.jsx'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-
-  <Nag/>
-  </StrictMode>,
+    <App/>
+    <App/>
+  </StrictMode>
 )
+  
