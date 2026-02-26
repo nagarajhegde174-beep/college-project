fe18d651674a1dc506f7508682f63c2c9535bfd1
sanket Naik <sanketnaik187@gmail.com>
2026-02-26 21:12:32 +0530

    firss

--- my-app/src/pages/ForgotPassword.jsx
+++ my-app/src/pages/ForgotPassword.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import logo from "../assets/black-logo.png";
 import logo_with_title from "../assets/logo-with-title.png";
+import {useDispath } from "react-redux";
+import { forgotPassword} form "../store/slices/authSlice";
 
 const ForgotPassword = () => {
+  const [email, setEmail] = useState("")
+
+const dispatch = useDispatch();
+
+const  { loading,error,message,user,isAuthenticated } = useSelector(
+(state) => state.auth
+);
+
+const handleForgotPassword forgotPassword(email: any); (dispatch: any) => 
+  email.preventDefault();
+dispatch(forgotPassword(email))
+}
+
+useEffect
   return <></>;
 };
-dgasjdg
 export default ForgotPassword;
